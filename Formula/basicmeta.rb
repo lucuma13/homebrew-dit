a224b3761471b79dc2a2afd7e8befb9a94c41f1b
lucuma13 <alterluigi@gmail.com>
2026-04-14 21:02:09 +0100

    updated version

--- Formula/basicmeta.rb
+++ Formula/basicmeta.rb
@@ -1,9 +1,9 @@
 class Basicmeta < Formula
   desc "A basic metadata utility for sanity checking original camera files (frame rate, resolution and encoded date)."
   homepage "https://github.com/lucuma13/basicmeta"
-  version "1.0"
-  url "https://github.com/lucuma13/basicmeta/archive/refs/tags/1.0.tar.gz"
-  sha256 "ed20c912728f943af965f0f7adbf864fe9a56024e93a03f6a23b448b96b4c3b9"
+  version "1.1"
+  url "https://github.com/lucuma13/basicmeta/archive/refs/tags/1.1.tar.gz"
+  sha256 "4374824191695c52985b788463ef6f7573ff202a07ff87cff6a758c9831270ae"
   license "GPL-3.0"
 
   # Dependencies
