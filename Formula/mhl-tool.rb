d7c457021bf5a7aef49549f6d1c302d87cc99745
Lucuma13 <alterluigi@gmail.com>
2026-04-15 03:30:18 +0100

    mhl-tool updated

--- Formula/mhl-tool.rb
+++ Formula/mhl-tool.rb
@@ -6,10 +6,10 @@ class MhlTool < Formula
 
   # Detect OS and fetch the corresponding raw binary
   if OS.mac?
-    url "https://github.com/lucuma13/mhl-tool/releases/download/1.31/mhl-macos"
+    url "https://github.com/lucuma13/mhl-tool/releases/download/1.31/mhl-tool-v1.31-macos"
     sha256 "7c13d1878ae47b745f44740692c7a7b499f95d0dd1367b181e15a981574122e6"
   elsif OS.linux?
-    url "https://github.com/lucuma13/mhl-tool/releases/download/1.31/mhl-linux"
+    url "https://github.com/lucuma13/mhl-tool/releases/download/1.31/mhl-tool-v1.31-linux"
     sha256 "7c7f59bad8ca0054920e612b5a79b8183152c4ddd854064217a945502044af5a"
   end
 
