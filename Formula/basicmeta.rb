f7acd8e97a894b0bbb1ad6d67a5be684a7d94a42
Lucuma13 <alterluigi@gmail.com>
2026-04-16 21:06:44 +0100

    update

--- Formula/basicmeta.rb
+++ Formula/basicmeta.rb
@@ -1,20 +1,18 @@
 class Basicmeta < Formula
-  desc "A basic metadata utility for sanity checking original camera files (frame rate, resolution and encoded date)."
+  desc "Lightweight metadata utility to perform quick checks on original camera files (OCF)"
   homepage "https://github.com/lucuma13/basicmeta"
-  version "1.1"
   url "https://github.com/lucuma13/basicmeta/archive/refs/tags/1.1.tar.gz"
   sha256 "4374824191695c52985b788463ef6f7573ff202a07ff87cff6a758c9831270ae"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
-  # Dependencies
-  depends_on "media-info"
   depends_on "exiftool"
+  depends_on "media-info"
 
   def install
     bin.install "basicmeta.sh" => "basicmeta"
   end
 
   test do
-      system "#{bin}/basicmeta", "--version"
+      system bin/"basicmeta", "--version"
   end
 end
