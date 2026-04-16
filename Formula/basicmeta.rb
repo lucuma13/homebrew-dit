0eb6bdd182bb306e17ca156d856128601b2b7f6e
Lucuma13 <alterluigi@gmail.com>
2026-04-16 21:21:44 +0100

    update

--- Formula/basicmeta.rb
+++ Formula/basicmeta.rb
@@ -1,5 +1,5 @@
 class Basicmeta < Formula
-  desc "Lightweight metadata utility to perform quick checks on original camera files (OCF)"
+  desc "Metadata utility for quick checking original camera files (OCF)"
   homepage "https://github.com/lucuma13/basicmeta"
   url "https://github.com/lucuma13/basicmeta/archive/refs/tags/1.1.tar.gz"
   sha256 "4374824191695c52985b788463ef6f7573ff202a07ff87cff6a758c9831270ae"
@@ -13,6 +13,6 @@ class Basicmeta < Formula
   end
 
   test do
-      system bin/"basicmeta", "--version"
+    system bin/"basicmeta", "--version"
   end
 end
