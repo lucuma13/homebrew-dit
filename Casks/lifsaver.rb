c5f79f142fdce65376c70a12e54b0df4b8061515
Lucuma13 <alterluigi@gmail.com>
2026-07-19 15:42:26 +0100

    lifsaver: fix download URL (no v prefix)

--- Casks/lifsaver.rb
+++ Casks/lifsaver.rb
@@ -2,7 +2,7 @@ cask "lifsaver" do
   version "0.1.0-beta.1"
   sha256 "6a3f3816f70ff6ae16029f73e0cb046d4f35c5b6d20651ad5bbb12c4b683efe0"
 
-  url "https://github.com/lucuma13/lifsaver/releases/download/v#{version}/lifsaver-#{version}-macos-universal.zip"
+  url "https://github.com/lucuma13/lifsaver/releases/download/#{version}/lifsaver-#{version}-macos-universal.zip"
   name "lifsaver"
   desc "Force-mount external camera cards stuck in macOS LIFS Disk Utility limbo"
   homepage "https://github.com/lucuma13/lifsaver"
